cad43c003eb3cb1d74e0d62059207dfe28468605
Andrija1911 <andrija.drk1911@gmail.com>
2018-08-28 13:22:16 +0100

    dc motor is now working

--- test_teensy_robot/dc_motor/dc_motor.ino
+++ test_teensy_robot/dc_motor/dc_motor.ino
@@ -1,5 +1,6 @@
 
 int PowerRobot=28;
+int disable_motors=27;
 
 int left_pwm_pin=3;
 int right_pwm_pin=4;
@@ -14,15 +15,25 @@ void setup()
   pinMode(right_dir_pin, OUTPUT);
   pinMode(left_pwm_pin, OUTPUT);
   pinMode(left_dir_pin, OUTPUT);
-  pinMode(PowerRobot, OUTPUT);
   
-  digitalWrite(right_dir_pin,LOW);
-  digitalWrite(left_dir_pin,LOW);
+  pinMode(PowerRobot, OUTPUT);
+  pinMode(disable_motors, OUTPUT);
+  
+  digitalWrite(right_dir_pin,HIGH);
+  digitalWrite(left_dir_pin,HIGH);
+  digitalWrite(right_pwm_pin,HIGH);
+  digitalWrite(left_pwm_pin,HIGH);
+  
   digitalWrite(PowerRobot,HIGH);
+  digitalWrite(disable_motors,HIGH);
+
+  analogWriteFrequency(right_pwm_pin, 20000);
+  analogWriteFrequency(left_pwm_pin, 20000);
+  analogWriteResolution(12);
   
   Serial.begin(9600);
   while (! Serial);
-  Serial.println("Speed 0 to 255");
+  Serial.println("Speed 0 to 4096");
 } 
  
  
@@ -32,11 +43,14 @@ void loop()
   {
     speed = Serial.parseInt();
     
-    if (speed >= 0 && speed <= 255)
+    if (speed >= 0 && speed <= 4096)
     {
       Serial.print(String(speed));
       analogWrite(right_pwm_pin, speed);
+      analogWrite(left_pwm_pin, speed);
       Serial.println(" -");
+    } else {
+      //digitalWrite(right_pwm_pin, LOW);  
     }
   }
 }
