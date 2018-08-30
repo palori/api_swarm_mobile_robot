4a28c470f59f6d9e44bae0b5426506d5153d5401
Andrija1911 <andrija.drk1911@gmail.com>
2018-08-30 17:58:43 +0100

    dc motor plus encoder working in example

--- test_teensy_robot/dc_motor/dc_motor.ino
+++ test_teensy_robot/dc_motor/dc_motor.ino
@@ -1,19 +1,24 @@
 #include <motor.h>
 #include <pins.h>
 #include <utils.h>
+#include <Encoder.h>
 
 Motor motor1(LEFT_MOTOR);
 Motor motor2(RIGHT_MOTOR);
 
+//encoder give 48 pulses per revolution
+//gearing ratio between shaft and encoder is 1:9.68
+Encoder encoder1(PIN_LEFT_ENCODER_A,PIN_LEFT_ENCODER_B);
+Encoder encoder2(PIN_RIGHT_ENCODER_A,PIN_RIGHT_ENCODER_B);
+
 int speed=4096;
+float revolutions1;
+float revolutions2;
 
 void setup() 
 { 
-  setUpPowerPins();
-  
-
-  Serial.begin(9600);
-  
+  setUpPowerPins(); 
+  Serial.begin(9600);  
   while (! Serial);
   Serial.println("Speed 0 to 4096");
 } 
@@ -28,12 +33,20 @@ void loop()
     if (speed >= 2048 && speed <= 4096)
     {
       enableMotors();
-      Serial.print(String(speed));
+      Serial.print("Speed: ");
+      Serial.println(String(speed));
       motor1.setVelocity(speed);
       motor2.setVelocity(speed);
+        
     } else {
       //digitalWrite(right_pwm_pin, LOW); 
       disableMotors(); 
     }
+    
+    revolutions1=encoder1.read()/48/9.68;
+    revolutions2=encoder2.read()/48/9.68;
+    Serial.print(revolutions1);
+    Serial.print("    ");
+    Serial.println(revolutions2);
   }
 }
