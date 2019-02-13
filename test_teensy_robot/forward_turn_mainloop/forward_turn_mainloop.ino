8e6f897a85b19f9e9485209397c3f8c755610d2b
Unknown <andrija.drk1911@gmail.com>
2019-02-13 10:49:28 +0100

    forward and turn functions now run in the main loop

--- test_teensy_robot/forward_turn_mainloop/forward_turn_mainloop.ino
+++ test_teensy_robot/forward_turn_mainloop/forward_turn_mainloop.ino
@@ -17,11 +17,9 @@ double gear_ratio = 9.68; //gearing ratio between shaft and encoder is 1:9.68
 //PID values
 double vel1 = 0.0;
 double vel2 = 0.0;
-double out_vel1 = 0.0;
-double out_vel2 = 0.0;
 double velocity1 = 0.0;
 double velocity2 = 0.0;
-double dist_error = 0.0;
+
 
 //PID constants
 double Kp = 1;
@@ -70,14 +68,19 @@ double c_r = 2 * PI * wheel_radius / pulses_per_rotation / gear_ratio;   //wheel
 double c_l = 2 * PI * wheel_radius / pulses_per_rotation / gear_ratio;
 
 //initial values
-//double x0;
-//double y0;
-//double Th0;
+double x_0;
+double y_0;
+double Th_0;
 double v_max;
 
-//forward function
+//forward and turn functions
 int starting_position = 1;
 int disableM = 0;
+double dist_error = 0.0;
+double dist_curr = 0.0;
+double angle_ref_abs = 0.0;
+double angle_error = 0.0;
+enum Command {TRN, FWD};
 
 double sign(double in){
   if (in < 0) return -1.0;
@@ -124,8 +127,6 @@ double getVelocity(int id, unsigned long newtime){
   return velocity[id-1];
 }    
 
-//measure time between pulses
-
 
 
 void updatePosition(double left_wheel_pos, double right_wheel_pos){
@@ -165,12 +166,13 @@ double PID(double referent_value, double current_value, int index){
 
 void forward(double dist_ref){
   
-    double x0 = odoX;
-    double y0 = odoY;
-    double Th0 = odoTh;  
+    x_0 = odoX;
+    y_0 = odoY;
+    Th_0 = odoTh;  
     
-    double dist_curr = sign(dist_ref) * sqrt(pow(odoX - x0 , 2.0) + pow(odoY - y0 , 2.0)); 
+    dist_curr = sign(dist_ref) * sqrt(pow(odoX - x_0 , 2.0) + pow(odoY - y_0 , 2.0)); 
     dist_error = dist_ref - dist_curr;
+    
     if (disableM == 0) enableMotors();
 
     initializePID(VEL1,Kp,Ki,0.01);
@@ -178,49 +180,14 @@ void forward(double dist_ref){
     initializePID(DIST,Kp_d,Ki_d,0.01);
     initializePID(THETA,Kp_Th,Ki_Th,0.01);
     
-    while (fabs(dist_error) > 0.03){
-      
-      //vel1 = PID(dist_ref,dist_curr,DIST) - PID(Th0,odoTh,THETA);
-      //vel2 = PID(dist_ref,dist_curr,DIST) + PID(Th0,odoTh,THETA);
-      
-      vel1=0.4 - PID(Th0,odoTh,THETA);
-      vel2=0.4 + PID(Th0,odoTh,THETA);
-      v_max=sign(dist_ref) * sqrt(fabs(dist_error));
-      
-      vel1 = Saturate(vel1 , v_max);        //saturation should be used just in case of reaching nominal speed, the control should implement steady state wanted speed
-      vel2 = Saturate(vel2 , v_max);
-
-      Serial.println("vel1: "+String(vel1));
-      Serial.println("vel2: "+String(vel2));
-        
-      delay(10);
-      velocity1 = getVelocity(LEFT_MOTOR,millis());
-      velocity2 = getVelocity(RIGHT_MOTOR,millis());
-      
-      updatePosition((double)encoder1.read(),(double)encoder2.read());   //check overflow
-      Serial.println("odoX: "+String(odoX));
-      Serial.println("odoY: "+String(odoY));
-      Serial.println("odoTh: "+String(WrapTo2PI(odoTh)));
-
-      motor1.setVelocity(PID(vel1,velocity1,VEL1));   //implement deacceleration
-      motor2.setVelocity(PID(vel2,velocity2,VEL2));
-      
-      dist_curr=sign(dist_ref) * sqrt(pow(odoX - x0 , 2.0)+pow(odoY - y0 , 2.0));
-      dist_error = dist_ref - dist_curr;
-      
-    }
-    disableMotors();
-    delay(500);
 }
 
 void turn(double angle_ref){
 
     if (fabs(angle_ref) > PI) angle_ref = 2 * PI - sign(angle_ref) * angle_ref; //makes sure to turn in the right direction
     
-    double Th0 = odoTh;
-    double angle_ref_abs = angle_ref + Th0;
-    double angle_error=0.0;
-
+    Th_0 = odoTh;
+    angle_ref_abs = angle_ref + Th_0;
     angle_error = angle_ref_abs - odoTh;
     
     if (disableM==0) enableMotors();
@@ -229,38 +196,66 @@ void turn(double angle_ref){
     initializePID(VEL1,Kp,Ki,0.01);
     initializePID(VEL2,Kp,Ki,0.01);
     
-    while (fabs(angle_error)>0.02){
-      
-      //vel2 = PID(angle_ref_abs,odoTh,TURN);
-      //vel1 = -PID(angle_ref_abs,odoTh,TURN);
+}
 
-      vel1 = - sign(angle_error) * 0.4;
-      vel2 = sign(angle_error) * 0.4;
-      
-      v_max = sign(angle_error) * fabs(sqrt(angle_error)); 
-      vel1 = Saturate(vel1 , v_max);   
-      vel2 = Saturate(vel2 , v_max);
-      
-      motor1.setVelocity(PID(vel1,velocity1,VEL1));   
-      motor2.setVelocity(PID(vel2,velocity2,VEL2));
+void update_velocity(Command RPI_command, double RPI_value){
+  
+  // forward:
 
-      //motor1.setVelocity(vel1);   
-      //motor2.setVelocity(vel2);
+  switch (RPI_command) {
+        case TRN:
+            if (fabs(angle_error)>0.01){
       
-      delay(10);
+                //vel2 = PID(angle_ref_abs,odoTh,TURN);
+                //vel1 = -PID(angle_ref_abs,odoTh,TURN);
+          
+                vel1 = - sign(angle_error) * 0.4;
+                vel2 = sign(angle_error) * 0.4;
+                
+                v_max = sign(angle_error) * fabs(sqrt(angle_error * wheels_distance / 2.0)); 
+                vel1 = Saturate(vel1 , v_max);   
+                vel2 = Saturate(vel2 , v_max);
+                
+                
+                angle_error = angle_ref_abs - odoTh;
+                Serial.println("angle error:"+String(angle_error));
+                
+            } else {
+                vel1 = 0.1;
+                vel2 = 0.1;
+                disableMotors();         
+            }
+          
+          break;
+          
+        case FWD:
+        
+            if (fabs(dist_error) > 0.01){
+      
+                //vel1 = PID(dist_ref,dist_curr,DIST) - PID(Th0,odoTh,THETA);
+                //vel2 = PID(dist_ref,dist_curr,DIST) + PID(Th0,odoTh,THETA);
+                
+                vel1=0.4 - PID(Th_0,odoTh,THETA);
+                vel2=0.4 + PID(Th_0,odoTh,THETA);
+                v_max=sign(RPI_value) * sqrt(fabs(dist_error));
+                
+                vel1 = Saturate(vel1 , v_max);        //saturation should be used just in case of reaching nominal speed, the control should implement steady state wanted speed
+                vel2 = Saturate(vel2 , v_max);
+          
+                Serial.println("vel1: "+String(vel1));
+                Serial.println("vel2: "+String(vel2));
+                  
+                dist_curr=sign(RPI_value) * sqrt(pow(odoX - x_0 , 2.0)+pow(odoY - y_0 , 2.0));
+                dist_error = RPI_value - dist_curr;
+            
+            } else {
+                vel1 = 0.1;
+                vel2 = 0.1;
+                disableMotors();       
+            }
+          break;
+  }
 
-      velocity1 = getVelocity(LEFT_MOTOR , millis());
-      velocity2 = getVelocity(RIGHT_MOTOR , millis());
-      updatePosition((double)encoder1.read() , (double)encoder2.read());   //check overflow, it should overflow when int/double overflows
-      Serial.println("odoX: "+String(odoX));
-      Serial.println("odoY: "+String(odoY));
-      Serial.println("odoTh: "+String(WrapTo2PI(odoTh))); 
-      
-      angle_error = angle_ref_abs - odoTh;
-      Serial.println("angle error:"+String(angle_error));
-    }
-    disableMotors();
-    delay(500);
 }
 
 double input=0.1;
@@ -274,24 +269,47 @@ void setup()
     
   motor1.setVelocity(input);   //sets motor to small speed where they dont move
   motor2.setVelocity(input);
+
+ 
       
 } 
 
+Command RPI_command = TRN;
+double RPI_value = 3.1415;
+bool newCommand = true;
+
+//turnr function: v2/v1=(R+b/2)/R
 
 void loop() 
 { 
-    forward(0.5);
-    turn(-1.57);
+
+    if (newCommand == true){
+      switch (RPI_command) {
+        case TRN:
+          turn(RPI_value);
+          break;
+        case FWD:
+          forward(RPI_value);
+          break;
+      }
+      newCommand = false;
+    }
     
-    forward(0.5);
-    turn(-1.57);
+    update_velocity(RPI_command,RPI_value);
+
+    motor1.setVelocity(PID(vel1,velocity1,VEL1));   
+    motor2.setVelocity(PID(vel2,velocity2,VEL2));
+      
+    delay(10);
+
+    velocity1 = getVelocity(LEFT_MOTOR , millis());
+    velocity2 = getVelocity(RIGHT_MOTOR , millis());
+    updatePosition((double)encoder1.read() , (double)encoder2.read());   //check overflow, it should overflow when int/double overflows
     
-    //disableM=1;
-    forward(0.5);
-    turn(-1.57);
+    Serial.println("odoX: "+String(odoX));
+    Serial.println("odoY: "+String(odoY));
+    Serial.println("odoTh: "+String(WrapTo2PI(odoTh))); 
+    Serial.println("v_max: "+String(v_max));
     
-    forward(0.5);
-    turn(-1.57);
-    disableM=1;
   
 }
