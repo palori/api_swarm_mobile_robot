4deedd2fe10cbd8a1fde06f8e145f3066e604d87
palori <paulopezribas@gmail.com>
2019-03-01 17:41:20 +0100

    line detection tests

--- API_rpi/image_proc/tests/detect_line/detect_line.cpp
+++ API_rpi/image_proc/tests/detect_line/detect_line.cpp
@@ -30,77 +30,37 @@ void display_image(Mat img, string title)
 }
 
 
-vector<Mat> blur_imgs(vector<Mat> imgs, vector<string> title){
-	vector<Mat> blu(imgs.size());
-	for (int i = 0; i < imgs.size(); ++i)
-	{
-		blur(imgs[i], blu[i], Size(5,5));
-		display_image(blu[i],title[i]);
-		string image_name = "images/img_" + title[i] + ".png";
-		imwrite(image_name, blu[i]);
-	}
-	return blu;
-
-}
-
-vector<Mat> binarize_imgs(vector<Mat> imgs, vector<string> title, vector<int> threshold_value){
-	vector<Mat> bin(imgs.size());
-	for (int i = 0; i < imgs.size(); ++i)
-	{
-		threshold( imgs[i], bin[i], threshold_value[i], max_BINARY_value,threshold_type );
-		display_image(bin[i],title[i]);
-		string image_name = "images/img_" + title[i] + ".png";
-		imwrite(image_name, bin[i]);
-	}
-	return bin;
-}
-
-//// NOT WORKING!!!
-vector<Mat> hist_imgs(vector<Mat> imgs, vector<string> title){
-	/// Establish the number of bins
-	int histSize = 256;
-
-	/// Set the ranges ( for B,G,R) )
-	float range[] = { 0, 256 } ;
-	const float* histRange = { range };
-
-	bool uniform = true; bool accumulate = false;
-
-	// Draw the histograms for R, G and B
-	int hist_w = 512; int hist_h = 400;
-	int bin_w = cvRound( (double) hist_w/histSize );
-	Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
-
-	vector<Mat> img_hist(imgs.size());
-
-	/// Compute the histograms:
-	for (int i = 0; i < imgs.size(); ++i)
-	{
-  		calcHist( &imgs[i], 1, 0, Mat(), img_hist[i], 1, &histSize, &histRange, uniform, accumulate );
-  		/// Normalize the result to [ 0, histImage.rows ]
-		normalize(img_hist[i], img_hist[i], 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-		for( int i = 1; i < histSize; i++ )
-		{
-			line( histImage, Point( bin_w*(i-1), hist_h - cvRound(img_hist[i].at<float>(i-1)) ) ,
-				Point( bin_w*(i), hist_h - cvRound(img_hist[i].at<float>(i)) ),
-				Scalar( 255, 0, 0), 2, 8, 0  );
-		}
-  		display_image(histImage,title[i]);
-		string image_name = "images/img_" + title[i] + ".png";
-		imwrite(image_name, histImage);
-  	}
-  	return img_hist;
-}
-
-
 int main( ){
+	// Take pic example
+	time_t timer_begin,timer_end;
+	raspicam::RaspiCam_Cv Camera;
+	Mat img;
+    int nCount=100;
+    //set camera params
+    Camera.set( CV_CAP_PROP_FORMAT, CV_8UC1 );
+    //Open camera
+    cout<<"Opening Camera..."<<endl;
+    if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
+    //Start capture
+    cout<<"Capturing "<<nCount<<" frames ...."<<endl;
+    time ( &timer_begin );
+    Camera.grab();
+    Camera.retrieve (img);
+    cout<<"Stop camera..."<<endl;
+    Camera.release();
 
-	string path = "../../../../test_images/bdrs_sally_images_20180302/images_20180302/";
-    string image_name = "image_20180302_161046.359_005.png";
-    //string image_name = "image_20180302_161159.774_015.png";
-    string image_path = path + image_name;
-    Mat img;
-    img = imread(image_path, CV_LOAD_IMAGE_COLOR);  
+    //show time statistics
+    time ( &timer_end ); /* get current time; same as: timer = time(NULL)  */
+    double secondsElapsed = difftime ( timer_end,timer_begin );
+    cout<< secondsElapsed<<" seconds for "<< nCount<<"  frames : FPS = "<<  ( float ) ( ( float ) ( nCount ) /secondsElapsed ) <<endl;
+    //save image 
+    cv::imwrite("pic.jpg",img);
+    cout<<"Image saved at 'pic.jpg'"<<endl;
+	
+
+
+    
+    //img = imread(image_path, CV_LOAD_IMAGE_COLOR);  
     cout << "Image size: " <<  img.size() << endl;
     cout << "Image channels: " <<  img.channels() << endl;
 
