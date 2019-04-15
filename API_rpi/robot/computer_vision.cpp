7a7c8616a9a502500d8f22c40a447dcec5cfe8d6
palori <paulopezribas@gmail.com>
2019-04-15 19:28:21 +0000

    fixed encode params

--- API_rpi/robot/computer_vision.cpp
+++ API_rpi/robot/computer_vision.cpp
@@ -240,11 +240,11 @@ string track_ball(Mat img){
 	sort(circles.begin(),circles.end(),compareCircles);
 
 	if (circles.size() == 0 ) 
-		return encode_image_params(BALL, false, 0.0, 0.0, 0);
+		return encode_image_params(BALL, false, 0.0, 0.0, 0,1);
 	else {
 		Vec3f ball = circles[0];
 		float theta = ball[0] - CAM_W / 2; 
-		return encode_image_params(BALL, true, 0.0, theta, 0) 
+		return encode_image_params(BALL, true, 0.0, theta, 0,1); 
 	}
 
 }
