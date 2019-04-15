37bb2e97620207c1a24c5cf1d9e7cd5dae57e9d9
palori <paulopezribas@gmail.com>
2019-04-15 13:46:12 +0200

    testing map1

--- API_rpi/robot/robot.cpp
+++ API_rpi/robot/robot.cpp
@@ -132,7 +132,7 @@ void Robot::serial(){
 
 			//---------------READING SERIAL---------------//
 			// Commented for testing in Pau's pc
-			cout << "reading serial..." << endl;
+			//cout << "reading serial..." << endl;
 			data = serial_comm.serial_read();
 			//cout << "\n*******\nserial data: " << data << "\n*******\n";
 			decode_sensors(data, sensors);
@@ -142,7 +142,7 @@ void Robot::serial(){
 				sensors.print_info();
 				count = 0;
 			}*/
-			sensors.print_info();
+			//sensors.print_info();
 			// save data
 			// need to be decoded to be used (can be done here or in localization...)
 			// maybe easier to modify 'comm_rpi_1.cpp' and do it there
@@ -278,6 +278,7 @@ void Robot::run(){
 				msg_task = encode_task(task);
 				pub_image_task.publish(msg_task);
 			}
+			navigate_test();
 		}
 
 	}
@@ -311,18 +312,20 @@ void Robot::navigate_test(){//Graph* map){
 		end = dijkstra.route.at(i);
 		edge = map->find_edge(start, end);
 		th_w = edge->get_th_w(start);
-		if (th_w != NULL){
+		//if (th_w != NULL){
 			//set msg to send to tsy
+			sensors.print_info();
+			cout << "-------------turn\n";
 			float trn = th_w - sensors.th.get_last_item();
 			string msg = "@i=20,a=16,b=1,v=0.4,trn=" + to_string(trn) + "$";
 			image_data.set(msg); 
 			this_thread::sleep_for(chrono::milliseconds(5000));
-
+			cout << "-------------fwd\n";
 			msg = "@i=21,a=15,b=1,v=0.4,fwd=" + to_string(edge->distance) + "$";
 			image_data.set(msg); 
 			this_thread::sleep_for(chrono::milliseconds(10000));
-
-
+			sensors.print_info();
+			cout << "-------------wait\n";this_thread::sleep_for(chrono::milliseconds(10000));
 			/* still to test
 			wait = true;
 			while (wait){
@@ -342,7 +345,7 @@ void Robot::navigate_test(){//Graph* map){
 				}
 				
 			}*/
-		}
-		else cout << "could not find the th_w" << endl;
+		//}
+		//else cout << "could not find the th_w" << endl;
 	}
 }
